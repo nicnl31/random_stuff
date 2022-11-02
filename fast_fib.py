5434b2b18368c0450135ef27f756c22070e551dd
Le Thanh Nhan <86213993+nicnl31@users.noreply.github.com>
2022-11-02 17:21:29 +0700

    Update fast_fib.py

--- fast_fib.py
+++ fast_fib.py
@@ -4,7 +4,9 @@ import time
 def dynamic_fib(n, memo={}):
     """Assumes n is an int >= 0, memo used only by recursive calls
        Returns Fibonacci of n"""
-    if n == 0 or n == 1:
+    if n == 0:
+        return 0
+    elif n == 1 or n == 2:
         return 1
     try:
         return memo[n]
